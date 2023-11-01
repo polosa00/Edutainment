837b38f3b9cae8ed02b3dc8bcce4d919bb644cf6
polosa00 <i@apolochanin.ru>
2023-11-01 19:55:20 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Александр Полочанин on 1.11.23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
