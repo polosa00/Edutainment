0ef4aab6bc2c099547f033fe45fd8dbc852f65d1
polosa00 <i@apolochanin.ru>
2023-11-02 01:26:43 +0300

    create full logic for app

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,16 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkingNumber = 1
+    @State private var numberOfQuestions = 10
+    @State private var isStartGame = false
+    
+    let numbers  = [5, 10, 15, 20]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color.blue.ignoresSafeArea()
+                Form {
+                    Section {
+                        
+                        VStack(alignment: .center, spacing: 15) {
+                            Text("What value do you want to check?")
+                                .bold()
+                                .font(.headline)
+                            Stepper("\(checkingNumber)", value: $checkingNumber, in: 1...9)
+                        }
+                       
+                        
+                        VStack(alignment: .center, spacing: 15) {
+                            Text("Choose number of questions")
+                                .bold()
+                                .font(.headline)
+                            Picker( "numbers", selection: $numberOfQuestions) {
+                                ForEach(numbers, id: \.self) { number in
+                                    Text("\(number)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                            
+                        }
+                        
+                       
+            
+                    } header: {
+                        Text("Settings")
+                            .font(.headline)
+                            .foregroundStyle(.indigo)
+                    }
+                    .padding(.horizontal, 10)
+                    
+
+                }
+                .navigationTitle("Edutainment")
+            .formStyle(.grouped)
+            }
+            
+            
+            NavigationLink {
+                QuestionsView(numberOfQuestions: numberOfQuestions, checkingNumber: checkingNumber)
+            } label: {
+                Text("Start game")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.indigo)
+                    .padding()
+                    .shadow(radius: 3)
+            }
+            .background(.orange)
         }
-        .padding()
     }
 }
+
+
+
 
 #Preview {
     ContentView()
--- Edutainment/QuestionsView.swift
+++ Edutainment/QuestionsView.swift
@@ -8,11 +8,150 @@
 import SwiftUI
 
 struct QuestionsView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var inputValue = ""
+    @State private var answerValue = 0
+    @State private var nextMultipleValue = 0
+    @State private var scoreValue = 0
+    @State private var showAlert = false
+    @State  var numberOfQuestions: Int
+    
+    let checkingNumber: Int
+    private let valuesForMultiply = [2, 3, 4, 5, 6, 7, 8, 9]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //            Color.red.ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: .indigo, location: 0.3),
+                .init(color: .blue, location: 0.3)
+            ], center: .top, startRadius: 250, endRadius: 700).ignoresSafeArea()
+            VStack (spacing: 30) {
+                Spacer()
+                Text("Score: \(scoreValue)")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(showAlert ? .indigo : .white)
+
+                VStack(alignment: .center, spacing: 20) {
+                    
+                    HStack(spacing: 15) {
+                        
+                        HStack {
+                            Text("\(checkingNumber)")
+                                .font(.title)
+                                .bold()
+                                .frame(width: 40)
+                                .foregroundStyle(.white)
+                            
+                            Text("x")
+                                .bold()
+                            
+                            Text("\(nextMultipleValue)")
+                                .font(.title)
+                                .bold()
+                                .frame(width: 40)
+                                .foregroundStyle(.white)
+                        }
+                        Text("?")
+                            .font(.title)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(.orange)
+                    .clipShape(.rect(cornerRadius: 20))
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    HStack {
+                        TextField("Enter your answer", text: $inputValue)
+                            .keyboardType(.numberPad)
+                            .textFieldStyle(.roundedBorder)
+                            .padding(.horizontal, 45)
+                    }
+                    
+                    Button {
+                        if numberOfQuestions > 1 {
+                            answerValue = Int(inputValue) ?? 1
+                            checkMultiple(checkingNumber, and: nextMultipleValue)
+                            inputValue = ""
+                        } else {
+                            inputValue = ""
+                            showAlert = true
+                        }
+                    } label: {
+                        
+                        Text("Check Answer")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(.indigo)
+                            .clipShape(.capsule)
+                    }
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    
+                }
+                .onAppear(perform:nextRandomMultipleValue)
+                .onSubmit {checkValue(value: inputValue)}
+                .frame(height: 300)
+                .frame(width: 300)
+                .padding(.horizontal, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Text("Count down of questions is: \(numberOfQuestions)")
+                    .foregroundStyle(.white)
+                    .font(.title3)
+                    .bold()
+                Spacer()
+                
+            }
+            
+        }
+        .toolbar(.hidden)
+        .alert(isPresented: $showAlert) {
+            Alert(
+                 title: Text("Questions is over"),
+                 message: Text("You score is: \(scoreValue)"),
+                 primaryButton: .default(Text("Try again"), action: {
+                     dismiss.callAsFunction()
+                 }),
+                 secondaryButton: .cancel()
+             )
+        }
+        
+        
+    }
+    
+    func checkValue(value: String) {
+        answerValue = Int(value) ?? 1
+    }
+    
+    func nextRandomMultipleValue() {
+        nextMultipleValue = valuesForMultiply.randomElement() ?? 1
+        
+    }
+    
+    func checkMultiple(_ numberOne: Int, and numberTwo: Int) {
+        let checkedValue = numberOne * numberTwo
+        if answerValue == checkedValue {
+            print(answerValue)
+            nextRandomMultipleValue()
+            countDown()
+            scoreValue += 1
+        } else {
+            scoreValue -= 1
+        }
+    }
+    
+    func countDown() {
+        numberOfQuestions -= 1
     }
 }
 
 #Preview {
-    QuestionsView()
+    QuestionsView(numberOfQuestions: 1, checkingNumber: 5)
 }
